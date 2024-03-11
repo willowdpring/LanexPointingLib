c93be4c247979d14317762c07863416805d9b3e1
willow <willowdpring@gmail.com>
2024-03-11 14:59:26 +0000

    reorganised espec calibs. need to hunt december data

--- pointing2d_espec.py
+++ pointing2d_espec.py
@@ -8,80 +8,76 @@ import os
 import pointing2d_fit as fit
 import csv
 
-folder = "D:\\Bunker C\\Lanex\\E_Spec_test_data\\042023_2a\\synced_with_spectrometer\\"
-files = walkDir(folder)
+
+folder = "D:\\Bunker C\\2022\\Dec 05\\Pointing Lanex\\Run005\\"
+#folder = "D:\\Bunker C\\Lanex\\E_Spec_test_data\\042023_2a\\synced_with_spectrometer\\"
+
+#files = walkDir(folder)
 
 saving = False
+
 
 exportDir = "{}\\EXPORTED".format(folder)  # name the subdirectory to export to
 if saving and not os.path.exists(exportDir):  # check if it exists
     os.mkdir(exportDir)  # create it if not
 
-GoodShot = files[8]
-Background = files[-1]
+#GoodShot = files[8]
+#Background = files[-1]
 
 threshold = 70
 
-plotfit = False
+plotfit = True
 
 x_fils = [3,5,53,5,3]
 
-kernel = norm_gaus2d_ary(5, 3, 9, 3)
+kernel = norm_gaus2d_ary(11, 3, 17, 3)
 
 """
 CALIBRATION
 """
-#  Fit results:
-"""
-a = 11.5
-b = 24170
-c = 32519703
-d = -2880
-e = 2150460
-"""
-
-a = 328.6443391832576
-b = -294684.42463444325
-c = 68563690.461126
-d = 785.6130708255362
-e = 167767.36631428992
 def ratio_of_quads(x,a,b,c,d,e):
     n = (a*x*x)+(b*x) + c
     d = (x*x) + (d*x) + e 
     y = n/d
     return(y)
 
+# espec calibration lookup
+# { 'DAY' : [a,b,c,d,e] }
+espec_coeffs = {
+    '22-12-05' : [11.5, 24170, 32519703, -2880, 2150460], ## ???? im doubtful of this calibration
+    '23-03-23' :[16.82203932056203,44381.734282360354,50860414.825793914,-2919.0964588725824,2204294.821475454],
+    '23-03-24' :[16.82203932056203,44381.734282360354,50860414.825793914,-2919.0964588725824,2204294.821475454],
+    '23-04-20' :[9.2080820556058758,35563.53137486182,4.48789354385e7,-2933.2162484668197,2281787.5593547565],
+    '23-04-26' :[9.280269716645035,36492.34932605621,48405689.11574387,-3352.4915825373205,2961512.5164166116],
+    '23-04-27' :[9.280351087922435,26752.558352428678,25734721.443078351683,-2345.4430,1453419.190032153],
+    '23-04-28' :[9.280351087922435,26752.558352428678,25734721.443078351683,-2345.4430,1453419.190032153]
+}
 
+plotcalibs = True
+if plotcalibs:
+    X = np.linspace(1,1000,1000)
+
+    fig,ax = plt.subplots(1,1)
+    for day in espec_coeffs.keys():
+        a,b,c,d,e = espec_coeffs[day]
+        ax.plot(X,ratio_of_quads(X,a,b,c,d,e),label=day)
+    ax.grid("both","both")
+    ax.legend(loc='best')
+
+    ax.set_xlabel("Pixel")
+    ax.set_ylabel("Energy")
+    ax.set_title(f"Espec Calibrations")
+    fig.show()
+
+a,b,c,d,e = espec_coeffs['22-12-05']
 
 bin = 1
 
-l_m = -0.18287037037037038
-l_c = 240.49305555555554
-
-"""analyse = {
-    files[8]:[[10,250],[800,450]],
-    files[22]:[[10,250],[800,450]],
-    files[23]:[[10,470],[800,670]],
-    files[24]:[[10,250],[800,450]],
-    files[26]:[[10,370],[800,570]],
-    files[28]:[[10,450],[800,650]],
-    files[29]:[[10,450],[800,650]]
-}"""
-
 analyse = {
-"D:/Bunker C/Lanex/E_Spec_test_data/042023_2a/synced_with_spectrometer/espec_r2-04202023153428-177.tiff": [[182, 238], [712, 534]],
-"D:/Bunker C/Lanex/E_Spec_test_data/042023_2a/synced_with_spectrometer/espec_r2-04202023153635-298.tiff": [[7, 349], [543, 647]],
-"D:/Bunker C/Lanex/E_Spec_test_data/042023_2a/synced_with_spectrometer/espec_r2-04202023153802-385.tiff": [[200, 260], [741, 533]],
-"D:/Bunker C/Lanex/E_Spec_test_data/042023_2a/synced_with_spectrometer/espec_r2-04202023154252-675.tiff": [[40, 505], [522, 743]],
-"D:/Bunker C/Lanex/E_Spec_test_data/042023_2a/synced_with_spectrometer/espec_r2-04202023154536-832.tiff": [[85, 307], [593, 655]],
-"D:/Bunker C/Lanex/E_Spec_test_data/042023_2a/synced_with_spectrometer/espec_r2-04202023154642-898.tiff": [[78, 207], [691, 583]],
-"D:/Bunker C/Lanex/E_Spec_test_data/042023_2a/synced_with_spectrometer/espec_r2-04202023154648-904.tiff": [[127, 449], [539, 659]],
-"D:/Bunker C/Lanex/E_Spec_test_data/042023_2a/synced_with_spectrometer/espec_r2-04202023154654-910.tiff": [[124, 229], [704, 572]],
-"D:/Bunker C/Lanex/E_Spec_test_data/042023_2a/synced_with_spectrometer/espec_r2-04202023154754-970.tiff": [[82, 377], [570, 619]],
-"D:/Bunker C/Lanex/E_Spec_test_data/042023_2a/synced_with_spectrometer/espec_r2-04202023154909-1045.tiff": [[191, 428], [683, 723]],
-"D:/Bunker C/Lanex/E_Spec_test_data/042023_2a/synced_with_spectrometer/espec_r2-04202023155100-1156.tiff": [[100, 434], [640, 756]]
+    "D:\\Bunker C\\2022\\Dec 05\\Pointing Lanex\\Run005\\HE\\Run005-12052022164625-530.tiff":[[250,450],[800,750]],
+    "D:\\Bunker C\\2022\\Dec 05\\Pointing Lanex\\Run005\\HE\\Run005-12052022171508-2240.tiff":[[200,450],[650,750]]
 }
-
+Background = "D:\\Bunker C\\2022\\Dec 05\\Pointing Lanex\\Run005\\HE\\Run005-12052022163736-1.tiff"
 
 fmodel = fit.setup_double_2d_gauss_model()
 
@@ -91,7 +87,8 @@ for file in analyse.keys():
     print(f"cropping {file.split('/')[-1]}  to \n x  [{crop[0][0]}   to  {crop[1][0]}] \n y  [{crop[0][1]}   to  {crop[1][1]}]")
 
     x_px = np.linspace(crop[0][0],crop[1][0],1+crop[1][0]-crop[0][0],endpoint=True)[::bin]
-    #e_x = ratio_of_quads(l_m*x_px+l_c,a,b,c,d,e)
+
+    e_x = ratio_of_quads(x_px,a,b,c,d,e)
     name = file[:-5].split('\\')[-1]
 
     shot = np.array(Image.open(file))[crop[0][1]:crop[1][1],crop[0][0]:crop[1][0]]
@@ -142,7 +139,7 @@ for file in analyse.keys():
     ax[0].set_yticks([])
     ax[1].plot(sub.sum(0))
     #print(ax[0].get_xticks())
-    labs = ["{:.0f}".format(ratio_of_quads(((l_m*(crop[0][0]+loc)+l_c)),a,b,c,d,e)) for loc in ax[0].get_xticks()] #e_x[int(loc)]) for loc in ax[0].get_xticks()] # 
+    labs = ["{:.0f}".format(e_x[int(loc)-crop[0][0]]) for loc in ax[0].get_xticks()] # ratio_of_quads(((l_m*(crop[0][0]+loc)+l_c)),a,b,c,d,e)) for loc in ax[0].get_xticks()]
     ax[1].set_xticklabels(labs)
     ax[1].grid('both','both')
     ax[1].set_xlabel("Energy in MeV")
@@ -157,7 +154,7 @@ for file in analyse.keys():
             writer = csv.writer(f)
             writer.writerow(["E","Q"])
             for i, q in enumerate(sub.sum(0)):
-                writer.writerow([e_x[i],q])
+                writer.writerow([ratio_of_quads((((crop[0][0]+i))),a,b,c,d,e),q])
 
         plt.close(fig)  
         
